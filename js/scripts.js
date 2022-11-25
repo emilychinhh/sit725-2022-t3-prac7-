45226d948e040145b8690c59937bba19c4a6e1b6
Emily Chin <emilyhui3823@gmail.com>
2022-11-25 15:20:40 +0800

    update-week3

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-function changeText(){
-    var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"] 
-    var number = getRandomNumberBetween(0,textsArray.length - 1) 
-    console.log("Index: ", number) 
-    document.getElementById("heading").innerHTML = textsArray[number];
-}
-function getRandomNumberBetween(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+// this scripts file is use to make the system dynamic, make changes in here not the server.js
+var express = require("express")
+var app = express()
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log("App listening to: "+port)
+})
+
