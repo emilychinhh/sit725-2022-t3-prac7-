7f3bfac8d827d2b55c69689441c052b42884b7a9
emilychinhh <emilyhui3823@gmail.com>
2022-12-10 16:23:22 +1100

    Wee

---
+++ models/project.js
@@ -0,0 +1,20 @@
+
+let client = require("../dbConnect"); // we need to make use this client here
+let projectCollection;
+
+setTimeout(() => {
+    projectCollection = client.mongoClient.db().collection("Project");
+}, 2000)
+
+//insert project...
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback); //calling projectCollection to insert the data into it
+}
+
+//get project
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+
+module.exports = {insertProjects, getProjects}
